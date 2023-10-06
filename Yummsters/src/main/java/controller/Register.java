a1c852fbfed109c38c5a72c38cff6f013f808e19
김희진 <95160590+Khjin06k@users.noreply.github.com>
2023-10-06 17:53:46 +0900

    Update Register.java

--- Yummsters/src/main/java/controller/Register.java
+++ Yummsters/src/main/java/controller/Register.java
@@ -34,7 +34,7 @@ public class Register extends HttpServlet {
         String content = "아무값이나 넣어보기";//multi.getParameter("editorContent");
         Integer food_category_id  = Integer.parseInt(multi.getParameter("food"));
         String[] store_category = multi.getParameterValues("store");
-        // 濡쒓렇�씤 �븳 �쉶�썝 議고쉶
+        // 로그인 한 회원 조회
         //HttpSession session = req.getSession();
         //Member member = (Member)session.getAttribute("user"); // TODO : 로그인 한 회원 key에 따라 변경 필요
         //String nickname = member.getNickname();
@@ -45,13 +45,13 @@ public class Register extends HttpServlet {
         board.setPicture(picture);
         board.setContent(content);
         board.setFood_category_id(food_category_id);
-        board.setNickname("�씗吏�");
+        board.setNickname("희진");
 
         try{
             // 등록 페이지에서 등록 버튼 클릭 시 DB 저장 후 메인 리스트 페이지로 이동
             BoardService boardService = new BoardServiceImpl();
             boardService.boardRegister(board);
-            res.sendRedirect("register.jsp");
+            res.sendRedirect("main_list.jsp");
             //req.getRequestDispatcher("main_list.jsp").forward(req, res);
         }catch (Exception e){
             // TODO : 에러가 날 경우 어떻게 처리할지 논의 필요
