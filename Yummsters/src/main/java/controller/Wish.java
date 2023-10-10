1e39fbfbae91c1f0f581dde6cafdbafe370b3c0b
gmlwls <khj4209kk@gmail.com>
2023-10-10 11:03:22 +0900

    ✨feat : 찜하기 버튼 기능 구현

---
+++ Yummsters/src/main/java/controller/Wish.java
@@ -0,0 +1,43 @@
+package controller;
+
+import bean.Member;
+import service.BoardService;
+import service.BoardServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/wish")
+public class Wish extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+
+        // 로그인한 회원 확인
+        HttpSession session = req.getSession();
+        Member member = (Member)session.getAttribute("member");
+
+        // 찜하기 버튼 클릭 시 넘겨준 게시글 아이디를 이용해 어떤 게시글인지 판별 후 찜하기 여부 처리
+        Integer board_id = Integer.parseInt(req.getParameter("board_id"));
+        try{
+            BoardService boardService = new BoardServiceImpl();
+            String response = boardService.boardWish(member.getNickname(), board_id);
+            res.getWriter().print(response);
+            System.out.println(response.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+            req.setAttribute("err", e.getMessage());
+            req.getRequestDispatcher("/error.jsp").forward(req, res);
+        }
+    }
+}
