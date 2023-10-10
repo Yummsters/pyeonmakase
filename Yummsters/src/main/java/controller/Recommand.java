9685d213083723dbc408f71687ac17a0c86985e5
gmlwls <khj4209kk@gmail.com>
2023-10-10 11:16:34 +0900

    ✨feat : 주석 수정

--- Yummsters/src/main/java/controller/Recommand.java
+++ Yummsters/src/main/java/controller/Recommand.java
@@ -16,18 +16,17 @@ import java.io.IOException;
 public class Recommand extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        // TODO : 추천 여부를 확인한 다음 추천을 했다면 별을 노란색으로, 하지 않았다면 검은색으로 변경하는 로직 생성
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        // TODO : 추천을 했는지 여부를 확인한 다음 추천을 했었다면 클릭 시 삭제, 안했었다면 클릭 시 추천하기 로직 생성
         req.setCharacterEncoding("utf-8");
 
+        // 로그인한 회원 확인
         HttpSession session = req.getSession();
         Member member = (Member)session.getAttribute("member");
 
-        // 추천하기 버튼 클릭 시 넘겨준 게시글 아이디를 이용해 어떤 게시글인지 판별
+        // 추천하기 버튼 클릭 시 넘겨준 게시글 아이디를 이용해 어떤 게시글인지 판별리
         Integer board_id = Integer.parseInt(req.getParameter("board_id"));
         try{
             BoardService boardService = new BoardServiceImpl();
