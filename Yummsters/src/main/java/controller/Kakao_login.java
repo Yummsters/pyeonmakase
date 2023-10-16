16b960a8b3c909e6eeff66e0e1c8bb00509b6c65
gmlwls <khj4209kk@gmail.com>
2023-10-16 11:06:39 +0900

    ✨feat : kakao 로그인 관련 코드 수정

--- Yummsters/src/main/java/controller/Kakao_login.java
+++ Yummsters/src/main/java/controller/Kakao_login.java
@@ -1,7 +1,6 @@
 package controller;
 
 import bean.Member;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 import service.MemberService;
 import service.MemberServiceImpl;
@@ -61,46 +60,5 @@ public class Kakao_login extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        /*try{
-            // 카카오에서 받은 응답으로 닉네임과 이메일 가져오기
-            ObjectMapper objectMapper = new ObjectMapper();
-            String kakaoLogin = req.getParameter("kakao");
-            Map<String, Object> kakaoData = objectMapper.readValue(kakaoLogin, Map.class);
-
-            Map<String, Object> kakaoAccount = (Map<String, Object>) kakaoData.get("kakao_account");
-            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
-
-            String nickname = (String) profile.get("nickname");
-            String email = (String) kakaoAccount.get("email");
-
-            // 데이터베이스에 있는지 확인 후 없을 경우 회원가입 진행
-            MemberService memberService = new MemberServiceImpl();
-            Member member = memberService.existMember(email, kakaoLogin);
-            System.out.println(member);
-
-            if(member == null){
-                member = new Member(nickname, nickname, email, email, "kakaoLogin");
-                memberService.signup(member);
-                response.put("signup", true);
-                response.put("login", true);
-            }else{
-                // 이미 회원인 경우 로그인 완료 진행
-                HttpSession session = req.getSession();
-                session.setAttribute("member", member);
-                req.getRequestDispatcher("home.jsp").forward(req, res);
-                response.put("signup", false);
-                response.put("login", true);
-            }
-
-            // JSON 형태로 변경
-            JSONObject jsonObject = new JSONObject(response);
-            res.setContentType("application/json");
-            res.getWriter().print(jsonObject.toJSONString());
-
-            System.out.println(jsonObject.toJSONString());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-*/
     }
 }
